d81ee2c01d21a25582901d07dfbd9a1369daa5c8
Afanas777 <serhii2111@yahoo.com>
2025-02-17 13:00:21 -0500

    message

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -14,6 +14,11 @@ function checkCountValue(){
     if (count === 10) {
         alert("Your Instagram post gained 10 followers! Congratulations!");
     } else if (count === 20){
-        alert("Your Instagram post gained 20 followers! Keep it up!");
-    }
+        alert("Your Instagram post gained 20 followers! Keep it up!"); 
+    } 
+}
+
+function resetCount(){
+    count = 0;
+    displayCount();
 }
