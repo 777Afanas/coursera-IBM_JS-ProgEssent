1d429eb72151a493cc5311b72dbca528e7255e35
777Afanas <serhii2111@yahoo.com>
2025-02-18 08:59:45 -0500

    message

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,6 +6,7 @@ function submitFeedback(){
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
+    const experince = document.getElementById('userExperince').value;
 
     alert('Thank you for valuable feedback'); 
 
@@ -16,10 +17,10 @@ function submitFeedback(){
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperinces').innerHTML = experince;
 
     document.getElementById('userInfo').style.display = 'block';    
-}
-
+} 
 
 const submitButton = document.getElementById('submitBtn'); 
 submitButton.onclick = submitFeedback;  
