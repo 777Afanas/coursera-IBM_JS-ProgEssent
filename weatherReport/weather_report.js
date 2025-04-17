b703705902f3ca01aa1f38ec0fb18b3418e1a8af
777Afanas <serhii2111@yahoo.com>
2025-04-17 12:24:12 -0400

    message

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -16,13 +16,35 @@ function showweatherDetails(event) {
     .catch(error => {
         console.error('Ошибка при получении погоды:', error);
         const weatherInfo = document.getElementById('weatherInfo');
-        weatherInfo.innerHTML = `<p>Не удалось получить погоду. Пожалуйста, попробуйте снова.</p>`;
+        weatherInfo.innerHTML = `<p>Не удалось получить погоду. Пожалуйста, попробуйте снова1.</p>`;
     });
 } 
 
-document.getElementById('weatherForm').addEventListener('submit', showweatherDetails);
+function showGeocoding(event) {
+    event.preventDefault();
+    const latitude = document.getElementById('latitude').value;
+    const longitude = document.getElementById('longitude').value;
+    const apiKey = '630f52faa7ee434510f43191450a8b40';  
+    
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metriс`;
+ 
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+        const weatherInfo = document.getElementById('weatherInfo') ;
+        weatherInfo.innerHTML = `<h2>Weatner in ${data.name}</h2>
+                               <p>Temperature: ${data.main.temp} &#8451;</p>     
+                               <p>Weather: ${data.weather[0].description}</p>`;
+    })
+    .catch(error => {
+        console.error('Ошибка при получении погоды:', error);
+        const weatherInfo = document.getElementById('weatherInfo');
+        weatherInfo.innerHTML = `<p>Не удалось получить погоду. Пожалуйста, попробуйте снова.</p>`;
+    });
+}
 
-
+document.getElementById('weatherForm').addEventListener('submit', showweatherDetails); 
+document.getElementById('weatherFormGeo').addEventListener('submit', showGeocoding);
 
 
 
